1f6985d76a26e9ae950db2227daf2c8f9f349e7f
postepenno <eugene.karataev@gmail.com>
2014-03-06 16:54:14 +0700

    flash games on postepenno

--- js/main.js
+++ js/main.js
@@ -15,42 +15,42 @@ function startup()
     var nsp = new Game(
         "New Splitter Pals",
         "assets/NSP.jpg",
-        "http://www.kongregate.com/games/EvgenyKarataev/new-splitter-pals",
+        "flash/nsp",
         "Splitter Pals are back! Cut your way through 40 brand new levels and create your own with the powerful level editor!"
     );
 
     var wutb5 = new Game(
         "Wake Up the Box 5",
         "assets/wutb5.jpg",
-        "http://www.kongregate.com/games/EvgenyKarataev/wake-up-the-box-5",
+        "flash/wutb5",
         "Those sleepy boxes are back and need to be woken up! Draw shapes on the play-field and let gravity take its course to knock the boxes awake."
     );
 
     var wutb4 = new Game(
         "Wake Up the Box 4",
         "assets/wutb4.jpg",
-        "http://www.kongregate.com/games/EvgenyKarataev/wake-up-the-box-4",
+        "flash/wutb4",
         "Draw shapes and let gravity take it's course in order to wake up each level's box in an all new approach to the hit game series."
     );
 
     var wutb3 = new Game(
         "Wake Up the Box 3",
         "assets/wutb3.jpg",
-        "http://www.kongregate.com/games/EvgenyKarataev/wake-up-the-box-3",
+        "flash/wutb3",
         "The Box is back and sleeping again. Find the ways to wake him up by placing objects on the playfield."
     );
 
     var sp = new Game(
         "Splitter Pals",
         "assets/sp.jpg",
-        "http://www.kongregate.com/games/EvgenyKarataev/splitter-pals",
+        "flash/sp",
         "Splitter is back with his pals!"
     );
 
     var sticks = new Game(
         "Sticks",
         "assets/sticks.jpg",
-        "http://www.kongregate.com/games/EvgenyKarataev/sticks",
+        "flash/sticks",
         "Bob is a usual office worker who dreams about beautiful life with fascinating women, sport cars and yachts. It seems that it is undoable dream, but one day..."
     );
 
@@ -63,37 +63,37 @@ function startup()
     var wtr = new Game(
         "Wake the Royalty",
         "assets/wtr.png",
-        "http://www.kongregate.com/games/EvgenyKarataev/wake-the-royalty",
+        "flash/wtr",
         "Wake the Royalty is the custom version of the game Wake Up the Box."
     );
     var wutb = new Game(
         "Wake Up the Box",
         "assets/wutb.jpg",
-        "http://www.kongregate.com/games/EvgenyKarataev/wake-up-the-box",
+        "flash/wutb",
         "Mr. Box sleeps too much. Wake him up in this physic based puzzle game."
     );
     var splitter2 = new Game(
         "Splitter 2",
         "assets/splitter2.jpg",
-        "http://www.kongregate.com/games/CasualCollective/splitter-2",
+        "flash/splitter2",
         "The sequel to Splitter: Cut your way through 32 levels and then take on the 100's of player created levels or make your own!"
     );
     var gen = new Game(
         "Gen",
         "assets/gen.jpg",
-        "http://www.kongregate.com/games/EvgenyKarataev/gen",
+        "flash/gen",
         "All of your precious spheres have been scattered far from the safety of the main sphere. It is up to you to gently guide them back."
     );
     var splitter = new Game(
         "Splitter",
         "assets/splitter.jpg",
-        "http://www.kongregate.com/games/EvgenyKarataev/splitter",
+        "flash/splitter",
         "In Splitter, each level can be solved in a number of different ways. Will you be able to come up with a brand new solution? Split the boxes and find out!"
     );
     var airbattle = new Game(
         "Air Battle",
         "assets/airbattle.jpg",
-        "http://www.kongregate.com/games/EvgenyKarataev/airbattle",
+        "flash/airbattle",
         "Guide your armed balloon through twenty challenging levels. Avoid accidents and shoot down any enemy balloons you encounter."
     );
 
