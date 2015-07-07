fc2b49ee1b89ef2755c57935feb880865b9a6ba4
karataev <island@ngs.ru>
2015-07-07 17:15:03 +0700

    New nav

--- ussr/js/game.js
+++ ussr/js/game.js
@@ -2,7 +2,7 @@
  * Created by postepenno on 21.05.2015.
  */
 
-var app = angular.module("app", ["duScroll", "toggle-switch", "ngSocial"]);
+var app = angular.module("app", ["duScroll", "toggle-switch", "ngSocial", "myNav"]);
 
 app.controller("MainCtrl", function($scope, $http) {
 
